d47b78a8b2a63a5c33fede999b6a43fb7fe9493d
Viktor Matveev <vikt0r@yandex.ru>
2025-02-15 13:00:46 +0300

    remarks ver.1 done + made fields final

--- src/ru/accounting_application/model/Transaction.java
+++ src/ru/accounting_application/model/Transaction.java
@@ -1,45 +1,37 @@
 package ru.accounting_application.model;
 
 public class Transaction {
-    private String name;
-    private boolean isExpense;
-    private int quantity;
-    private int unitPrice;
-
-    // TODO: 15.02.2025 Не должно быть неиспользуемого кода
-    public Transaction(String name, boolean isExpense, int quantity, int unitPrice) {
-        this.name = name;
-        this.isExpense = isExpense;
-        this.quantity = quantity;
-        this.unitPrice = unitPrice;
-    }
+    private final String NAME;
+    private final boolean IS_EXPENSE;
+    private final int QUANTITY;
+    private final int UNIT_PRICE;
 
     public Transaction(String transactionDataLine) {
         String[] transactionData = transactionDataLine.split(",");
-        this.name = transactionData[0];
-        this.isExpense = transactionData[1].equals("TRUE");
-        this.quantity = Integer.parseInt(transactionData[2]);
-        this.unitPrice = Integer.parseInt(transactionData[3]);
+        this.NAME = transactionData[0];
+        this.IS_EXPENSE = transactionData[1].equals("TRUE");
+        this.QUANTITY = Integer.parseInt(transactionData[2]);
+        this.UNIT_PRICE = Integer.parseInt(transactionData[3]);
     }
 
     public String getName() {
-        return this.name;
+        return NAME;
     } // TODO: 15.02.2025 Можно не писать
 
     public boolean getIsExpense() {
-        return this.isExpense;
+        return IS_EXPENSE;
     }
 
     public int getQuantity() {
-        return this.quantity;
+        return QUANTITY;
     }
 
     public int getUnitPrice() {
-        return this.unitPrice;
+        return UNIT_PRICE;
     }
 
     public String toString() {
-        return this.name + " - " + this.unitPrice * this.quantity;
+        return NAME + " - " + UNIT_PRICE * QUANTITY;
     }
 
 }
